c79824d1eae65d4195e18eb8cbbf2b6156bc8e15
caoguanjie <374348532@qq.com>
2018-06-30 10:26:42 +0800

    完成了自定义弹窗所有样式

--- src/pages/demo/modal-animate-demo/modal-scale/modal-scale.ts
+++ src/pages/demo/modal-animate-demo/modal-scale/modal-scale.ts
@@ -26,14 +26,18 @@ export class ModalScalePage {
     this.items = [
       {name:"选项一",isSelect:false},
       {name:"选项2",isSelect:false},
-      {name:"选项3",isSelect:false},
-      {name:"选项4",isSelect:false}
+      // {name:"选项3",isSelect:false},
+      // {name:"选项4",isSelect:false},
+      // {name:"选项5",isSelect:false},
+      // {name:"选项6",isSelect:false},
+      // {name:"选项7",isSelect:false}
     ]
   }
   dismiss() {
     this.viewCtrl.dismiss();
   }
-  changeValue(item){
+  changeValue(e,item){
+    e.stopPropagation();
     item.isSelect = !item.isSelect;
   }
 }
